9c2b759f3ebeecad7d1a47ac986da94f4129db7c
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2023-03-10 19:01:38 -0300

    (Página Login) Feito toda estrutura HTML e CSS, as imagens e cores já estão colocadas em pastas separadas.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import Login from './views/Login/Login'
-import './index.css'
+import './index.scss'
 
 const router = createBrowserRouter([
   {
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import "./index.scss"
+import React from "react";
+import "./index.scss";
+import Header from "../../Components/Header/Header";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
+      <section className="login">
+        <Header />
+        <form className="login__form">
+          <h1>Acesse com seu login ou cadastre-se!</h1>
+          <h2>você pode entrar com o seu CPF</h2>
+          <div className="login__input">
+            <label htmlFor="name">Digite seu CPF:</label>
+            <input
+              type="text"
+              placeholder="Nome completo"
+              id="name"
+              className="login__input-name"
+            />
 
-        <button>Oi</button>
-
+            <label htmlFor="passoword">Senha:</label>
+            <input type="passoword" id="passoword" placeholder="***********" />
+          </div>  
+          <button type="submit">
+            <Link to={"/home"}>Entrar</Link>
+          </button>
+        </form>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
